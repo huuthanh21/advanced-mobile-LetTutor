12b27ccdf146482e2d95b572683657ee76c4b3c9
20120582 <newacc2312@gmail.com>
2023-11-29 23:20:45 +0700

    Add random data getter to Tutor and User model

--- lib/models/tutor.dart
+++ lib/models/tutor.dart
@@ -1,3 +1,5 @@
+import 'package:faker/faker.dart';
+
 import 'user.dart';
 
 class Tutor {
@@ -24,15 +26,68 @@ class Tutor {
     required this.experience,
   });
 
+  static List<String> specializationList = [
+    'Tiếng Anh cho trẻ em',
+    'Tiếng Anh cho công việc',
+    'Giao tiếp',
+    'STARTERS',
+    'MOVERS',
+    'FLYERS',
+    'KET',
+    'PET',
+    'IELTS',
+    'TOEFL',
+    'TOEIC'
+  ];
+
   static Tutor getRandomTutor() {
-    return Tutor(
-      name: "Nguyen Van A",
-      description: "I am a teacher",
-      countryCode: "vn",
-      education: "Bachelor",
-      hobbies: "Reading",
-      experience: "5 years",
+    Tutor tutor = Tutor(
+      name: faker.person.name(),
+      description: faker.lorem.sentences(2).join(" "),
+      countryCode: faker.randomGenerator.element(["vn", "us", "jp", "kr"]),
+      education: faker.lorem.words(3).join(" "),
+      hobbies: faker.lorem.sentence(),
+      experience: faker.lorem.sentence(),
     );
+    // Add random languages
+    for (var i = 0; i < faker.randomGenerator.integer(3, min: 1); i++) {
+      String randomLanguage = faker.randomGenerator.element([
+        "English",
+        "Vietnamese",
+        "Japanese",
+        "Korean",
+        "Mandarin",
+        "French",
+        "German",
+        "Spanish"
+      ]);
+      if (!tutor.languages.contains(randomLanguage)) {
+        tutor.languages.add(randomLanguage);
+      }
+    }
+    // Add random specializations
+    for (var i = 0; i < faker.randomGenerator.integer(6, min: 1); i++) {
+      String randomSpecialization = faker.randomGenerator.element(
+          specializationList
+              .where((element) => !tutor.specializations.contains(element))
+              .toList());
+      tutor.specializations.add(randomSpecialization);
+    }
+    // Add random courses
+    for (var i = 0; i < faker.randomGenerator.integer(6, min: 1); i++) {
+      String randomCourse = faker.lorem.words(3).join(" ");
+      tutor.courses.add(randomCourse);
+    }
+    // Add random reviews
+    for (var i = 0; i < faker.randomGenerator.integer(6, min: 1); i++) {
+      Review randomReview = Review(
+          author: User.getRandomUser(),
+          content: faker.lorem.sentences(2).join(" "),
+          rating: faker.randomGenerator.integer(5, min: 1),
+          date: faker.date.dateTime());
+      tutor.reviews.add(randomReview);
+    }
+    return tutor;
   }
 }
 
@@ -49,9 +104,10 @@ class Review {
   final int rating;
   final DateTime date;
 
-  Review(
-      {required this.author,
-      required this.content,
-      required this.rating,
-      required this.date});
+  Review({
+    required this.author,
+    required this.content,
+    required this.rating,
+    required this.date,
+  });
 }
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,13 @@
+import 'package:faker/faker.dart';
+
 class User {
   String email;
   String password;
 
   User({required this.email, required this.password});
+
+  static User getRandomUser() {
+    return User(
+        email: faker.internet.email(), password: faker.internet.password());
+  }
 }
