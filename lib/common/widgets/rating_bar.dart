2788a9e5c2855cd5fa09a0d1f0ff703c73e7753d
20120582 <newacc2312@gmail.com>
2023-11-30 15:55:39 +0700

    Add custom size parameter to RatingBar

--- lib/common/widgets/rating_bar.dart
+++ lib/common/widgets/rating_bar.dart
@@ -2,22 +2,27 @@ import 'package:flutter/material.dart';
 
 class RatingBar extends StatelessWidget {
   final int _rating;
+  final double _size;
 
-  RatingBar({super.key, required int rating}) : _rating = rating.clamp(0, 5);
+  RatingBar({super.key, required int rating, double size = 20})
+      : _rating = rating.clamp(0, 5),
+        _size = size;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         for (int i = 0; i < _rating; i++)
-          const Icon(
+          Icon(
             Icons.star,
             color: Colors.yellow,
+            size: _size ?? 20,
           ),
         for (int i = 0; i < 5 - _rating; i++)
-          const Icon(
+          Icon(
             Icons.star,
-            color: Colors.grey,
+            color: Colors.grey.shade200,
+            size: _size ?? 20,
           ),
       ],
     );
