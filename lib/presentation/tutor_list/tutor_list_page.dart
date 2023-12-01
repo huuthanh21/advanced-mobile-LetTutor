3d34b769a3eddcb7e328d89c079e0d1fb39352d1
20120582 <newacc2312@gmail.com>
2023-12-01 14:08:56 +0700

    Move TutorCard widget to a new file

--- lib/presentation/tutor_list/tutor_list_page.dart
+++ lib/presentation/tutor_list/tutor_list_page.dart
@@ -1,14 +1,11 @@
-import 'package:country_flags/country_flags.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 import '../../common/providers/data_provider.dart';
-import '../../common/utils/country_mapper.dart';
 import '../../common/widgets/footer.dart';
-import '../../common/widgets/rating_bar.dart';
 import '../../common/widgets/top_app_bar.dart';
 import '../../models/tutor.dart';
+import 'components/tutor_card.dart';
 
 class TutorListPage extends StatelessWidget {
   const TutorListPage({super.key});
@@ -268,167 +265,6 @@ class TutorListPage extends StatelessWidget {
             const Footer(),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class TutorCard extends StatelessWidget {
-  const TutorCard({
-    super.key,
-    required this.tutor,
-  });
-
-  final Tutor tutor;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.white,
-        border: Border.all(
-          color: Colors.grey.withOpacity(0.2),
-        ),
-      ),
-      height: 400,
-      padding: const EdgeInsets.all(10),
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Header
-          Flexible(
-            flex: 3,
-            fit: FlexFit.tight,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 40,
-                      height: 40,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(40),
-                        child: Image.network(tutor.profilePictureUrl),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        InkWell(
-                          onTap: () => context.push(
-                              // TODO: Navigate to tutor detail page
-                              context.namedLocation("tutor")),
-                          child: Text(tutor.name,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .titleLarge!
-                                  .copyWith(fontSize: 22)),
-                        ),
-                        Row(children: [
-                          CountryFlag.fromCountryCode(tutor.countryCode,
-                              width: 20, height: 20),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            CountryMapper.countryCodeToName(tutor.countryCode),
-                            style: TextStyle(color: Colors.grey.shade600),
-                          ),
-                        ]),
-                        if (tutor.reviews.isEmpty)
-                          Text(
-                            "Không có đánh giá",
-                            style: TextStyle(
-                                color: Colors.grey.shade600,
-                                fontStyle: FontStyle.italic),
-                          )
-                        else
-                          RatingBar(rating: tutor.rating),
-                      ],
-                    )
-                  ],
-                ), // Header left
-                IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.favorite_border_outlined))
-              ],
-            ),
-          ),
-          // const SizedBox(
-          //   height: 10,
-          // ),
-          Flexible(
-            flex: 3,
-            fit: FlexFit.tight,
-            child: Wrap(
-              direction: Axis.horizontal,
-              spacing: 10,
-              runSpacing: 10,
-              children: [
-                for (String specialization in tutor.specializations)
-                  TextButton(
-                    onPressed: null,
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.grey.withOpacity(0.2),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                    ),
-                    child: Text(specialization,
-                        style: TextStyle(color: Colors.grey.shade800)),
-                  ),
-              ],
-            ),
-          ),
-          // const SizedBox(
-          //   height: 10,
-          // ),
-          Flexible(
-            flex: 4,
-            fit: FlexFit.tight,
-            child: Text(
-              tutor.description,
-              softWrap: true,
-              maxLines: 4,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(color: Colors.grey.shade600, fontSize: 16),
-            ),
-          ),
-          // const SizedBox(
-          //   height: 20,
-          // ),
-          Flexible(
-            flex: 2,
-            fit: FlexFit.tight,
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: TextButton.icon(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    side: BorderSide(
-                      color: Theme.of(context).colorScheme.primary,
-                      width: 1,
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 30, vertical: 10),
-                  ),
-                  icon: const Icon(Icons.calendar_month_outlined),
-                  label: const Text("Đặt lịch")),
-            ),
-          ),
-        ],
       ),
     );
   }
