b588ad3dd6e9d5b6fd66de61324551469ef56b34
20120582 <newacc2312@gmail.com>
2023-12-27 15:19:16 +0700

    Implement changing tabs in CoursesPage

--- lib/presentation/courses/courses_page.dart
+++ lib/presentation/courses/courses_page.dart
@@ -1,11 +1,151 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../common/widgets/footer.dart';
 import '../../common/widgets/top_app_bar_content.dart';
 
-class CoursesPage extends StatelessWidget {
+class CoursesPage extends StatefulWidget {
   const CoursesPage({super.key});
+
+  @override
+  State<CoursesPage> createState() => _CoursesPageState();
+}
+
+class _CoursesPageState extends State<CoursesPage> with SingleTickerProviderStateMixin {
+  static const List<Tab> tabs = [
+    Tab(text: 'Khoá học'),
+    Tab(text: 'Ebook'),
+    Tab(text: 'Interactive E-book'),
+  ];
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      initialIndex: 0,
+      length: 3,
+      vsync: this,
+    );
+    _tabController.addListener(() {
+      if (_tabController.indexIsChanging) {
+        setState(() {});
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  Widget _getTabAtIndex(int index) {
+    switch (index) {
+      case 0:
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'English for Traveling',
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 26,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const Gap(20),
+            Wrap(
+              spacing: 20,
+              runSpacing: 30,
+              children: [
+                for (int i = 0; i < 5; i++)
+                  InkWell(
+                    onTap: () => context.push(context.namedLocation('course')),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Theme.of(context).colorScheme.tertiary.withOpacity(0.5),
+                            blurRadius: 0,
+                            offset: const Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Container(
+                          width: 286,
+                          height: 370,
+                          // border and shadow
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(
+                              color: Theme.of(context).colorScheme.tertiary,
+                              width: 1,
+                            ),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                "assets/images/course_photo.png",
+                                width: 286,
+                              ),
+                              const Gap(10),
+                              Container(
+                                padding: const EdgeInsets.fromLTRB(24, 24, 24, 10),
+                                child: const Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("Life in the Internet Age",
+                                        style: TextStyle(
+                                          fontFamily: 'Poppins',
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w600,
+                                        )),
+                                    Gap(10),
+                                    Text(
+                                      "Let's discuss how technology is changing the way we live",
+                                      style: TextStyle(
+                                        fontFamily: 'Poppins',
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    Gap(10),
+                                    Text(
+                                      "Intermediate • 9  Lessons",
+                                      style: TextStyle(
+                                        fontFamily: 'Open Sans',
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ],
+        );
+      case 1:
+        return const Text('Ebook');
+      case 2:
+        return const Text('Interactive E-book');
+      default:
+        return const Text(''); // should never happen
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,9 +172,7 @@ class CoursesPage extends StatelessWidget {
                             Icons.school,
                             size: 100,
                           ),
-                          const SizedBox(
-                            width: 20,
-                          ),
+                          const Gap(20),
                           // Big icon and a column
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
@@ -90,9 +228,7 @@ class CoursesPage extends StatelessWidget {
                           ),
                         ],
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
+                      const Gap(10),
                       const Text(
                         'LiveTutor đã xây dựng nên các khóa học của các lĩnh vực trong cuộc sống chất lượng, bài bản và khoa học nhất cho những người đang có nhu cầu trau dồi thêm kiến thức về các lĩnh vực.',
                         style: TextStyle(
@@ -168,122 +304,16 @@ class CoursesPage extends StatelessWidget {
                       ),
                     ],
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  DefaultTabController(
-                    length: 3,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const TabBar(tabs: [
-                          Tab(text: 'Khoá học'),
-                          Tab(text: 'Ebook'),
-                          Tab(text: 'Interactive E-book'),
-                        ]),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        const Text(
-                          'English for Traveling',
-                          style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontSize: 26,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Wrap(
-                          spacing: 20,
-                          runSpacing: 30,
-                          children: [
-                            for (int i = 0; i < 5; i++)
-                              InkWell(
-                                onTap: () => context.push(context.namedLocation('course')),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(15),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color:
-                                            Theme.of(context).colorScheme.tertiary.withOpacity(0.5),
-                                        blurRadius: 0,
-                                        offset: const Offset(0, 4),
-                                      ),
-                                    ],
-                                  ),
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(15),
-                                    child: Container(
-                                      width: 286,
-                                      height: 370,
-                                      // border and shadow
-                                      decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius: BorderRadius.circular(15),
-                                        border: Border.all(
-                                          color: Theme.of(context).colorScheme.tertiary,
-                                          width: 1,
-                                        ),
-                                      ),
-                                      child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          Image.asset(
-                                            "assets/images/course_photo.png",
-                                            width: 286,
-                                          ),
-                                          const SizedBox(
-                                            height: 10,
-                                          ),
-                                          Container(
-                                            padding: const EdgeInsets.fromLTRB(24, 24, 24, 10),
-                                            child: const Column(
-                                              crossAxisAlignment: CrossAxisAlignment.start,
-                                              children: [
-                                                Text("Life in the Internet Age",
-                                                    style: TextStyle(
-                                                      fontFamily: 'Poppins',
-                                                      fontSize: 18,
-                                                      fontWeight: FontWeight.w600,
-                                                    )),
-                                                SizedBox(
-                                                  height: 10,
-                                                ),
-                                                Text(
-                                                  "Let's discuss how technology is changing the way we live",
-                                                  style: TextStyle(
-                                                    fontFamily: 'Poppins',
-                                                    fontSize: 12,
-                                                    fontWeight: FontWeight.w500,
-                                                  ),
-                                                ),
-                                                SizedBox(
-                                                  height: 10,
-                                                ),
-                                                Text(
-                                                  "Intermediate • 9  Lessons",
-                                                  style: TextStyle(
-                                                    fontFamily: 'Open Sans',
-                                                    fontSize: 16,
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                          ],
-                        ),
-                      ],
-                    ),
+                  const Gap(20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TabBar(
+                        controller: _tabController,
+                        tabs: tabs,
+                      ),
+                      _getTabAtIndex(_tabController.index),
+                    ],
                   ),
                 ],
               ),
